6cf89b682cea09a9ccbb55154d1dce647df5bef6
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-05-31 12:29:38 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,6 @@
+num_of_hw = 10
+num_of_hs = 3
+courses = 'python'
+time_for_one = (num_of_hs / num_of_hw)/num_of_hs
+
+print('Курс: ', courses, ', ' 'Всего задач: ',num_of_hw, ', ','среднее время выполнения: ', time_for_one)
